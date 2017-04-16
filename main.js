7b9ff2f8f101e0f8ff6d0fd7732ae817d06fcc95
Jevin Studios <info@jevinstudios.net>
2017-04-17 00:30:41 +0200

    Added function to the buy and sell buttons

--- main.js
+++ main.js
@@ -40,52 +40,94 @@ var RAM_7 = "32 GB";
 var RAM_7_cost = 0;
 var RAM_8 = "64 GB";
 var RAM_8_cost = 0;
-var HDD_1 = "16 GB";
-var HDD_2 = "32 GB";
-var HDD_3 = "64 GB";
-var HDD_4 = "128 GB";
-var HDD_5 = "256 GB";
-var HDD_6 = "512 GB";
-var HDD_7 = "1024 GB";
-var HDD_8 = "1128 GB";
-var HDD_9 = "1256 GB";
-var HDD_10 = "1512 GB";
+var HDD_1 = "120 GB";
+var HDD_1_cost = 0;
+var HDD_2 = "320 GB";
+var HDD_2_cost = 0;
+var HDD_3 = "500 GB";
+var HDD_3_cost = 0;
+var HDD_4 = "1 TB";
+var HDD_4_cost = 0;
+var HDD_5 = "2 TB";
+var HDD_5_cost = 0;
+var HDD_6 = "3 TB";
+var HDD_6_cost = 0;
+var HDD_7 = "4 TB";
+var HDD_7_cost = 0;
+var HDD_8 = "5 TB";
+var HDD_8_cost = 0;
 var videoCard_1 = "128 MB";
+var videoCard_1_cost = 0;
 var videoCard_2 = "256 MB";
+var videoCard_2_cost = 0;
 var videoCard_3 = "512 MB";
+var videoCard_3_cost = 0;
 var videoCard_4 = "1 GB";
+var videoCard_4_cost = 0;
 var videoCard_5 = "2 GB";
+var videoCard_5_cost = 0;
 var videoCard_6 = "4 GB";
+var videoCard_6_cost = 0;
 var videoCard_7 = "8 GB";
+var videoCard_7_cost = 0;
 var videoCard_8 = "16 GB";
+var videoCard_8_cost = 0;
 var videoCard_9 = "24 GB";
+var videoCard_9_cost = 0;
 var videoCard_10 = "32 GB";
+var videoCard_10_cost = 0;
 var screenSize_1 = '16"';
+var screenSize_1_cost = 0;
 var screenSize_2 = '18"';
+var screenSize_2_cost = 0;
 var screenSize_3 = '20"';
+var screenSize_3_cost = 0;
 var screenSize_4 = '22"';
+var screenSize_4_cost = 0;
 var screenSize_5 = '24"';
+var screenSize_5_cost = 0;
 var screenSize_6 = '26"';
+var screenSize_6_cost = 0;
 var screenSize_7 = '28"';
+var screenSize_7_cost = 0;
 var screenSize_8 = '30"';
+var screenSize_8_cost = 0;
 var screenSize_9 = '32"';
+var screenSize_9_cost = 0;
 var screenSize_10 = '34"';
+var screenSize_10_cost = 0;
 var screenSize_11 = '36"';
+var screenSize_11_cost = 0;
 var screenSize_12 = '38"';
+var screenSize_12_cost = 0;
 var screenSize_13 = '40"';
+var screenSize_13_cost = 0;
 var RPM_1 = "1500 RPM";
+var RPM_1_cost = 0;
 var RPM_2 = "2000 RPM";
+var RPM_2_cost = 0;
 var RPM_3 = "2500 RPM";
+var RPM_3_cost = 0;
 var RPM_4 = "3000 RPM";
+var RPM_4_cost = 0;
 var noiseLevel_1 = "40 dbA";
+var noiseLevel_1_cost = 0;
 var noiseLevel_2 = "35 dbA";
+var noiseLevel_2_cost = 0;
 var noiseLevel_3 = "30 dbA";
+var noiseLevel_3_cost = 0;
 var noiseLevel_4 = "25 dbA";
+var noiseLevel_4_cost = 0;
 var noiseLevel_5 = "20 dbA";
+var noiseLevel_5_cost = 0;
 var extras_speakers = "Speakers";
+var extras_speakers_cost = 0;
 var extras_webcam = "Webcam";
+var extras_webcam_cost = 0;
 var extras_curved = "Curved Display";
+var extras_curved_cost = 0;
 var extras_touchscreen = "Touchscreen";
+var extras_touchscreen_cost = 0;
 
 
 
@@ -129,4 +171,22 @@ for (i = 0; i < acc.length; i++) {
             panel.style.maxHeight = panel.scrollHeight + "px";
         }
     }
+}
+
+function buyComputer() {
+	if(computerManufactureCost <= money) {
+		money = money - computerManufactureCost;
+		computersInStock = computersInStock + 1;
+		document.getElementById("money").innerHTML = money;
+		document.getElementById("computersInStock").innerHTML = computersInStock;
+	}
+}
+
+function sellComputer() {
+	if(!(computersInStock == 0)) {
+		money = money + computerSellPrice;
+		computersInStock = computersInStock - 1;
+		document.getElementById("money").innerHTML = money;
+		document.getElementById("computersInStock").innerHTML = computersInStock;
+	}
 }
